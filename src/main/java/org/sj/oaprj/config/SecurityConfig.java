20f917754b137ecfb268ba08aa9458b6b0c828c6
dzfang <278481756@qq.com>
2016-04-24 20:24:15 +0800

    设定配置，允许使用Iframe

--- src/main/java/org/sj/oaprj/config/SecurityConfig.java
+++ src/main/java/org/sj/oaprj/config/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		http.headers().frameOptions().disable();
 		http.authorizeRequests()
 			.antMatchers("/login", "/css/**", "/js/**", "/img/**", "/api/**", "/error")
 			.permitAll()
